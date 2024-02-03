c75ca0e69789dcf2e26f6794494c659f7a31c301
Justin Ruiter <justin@justinruiter.nl>
2024-02-03 17:39:11 +0100

    Abstracted hashing code to Hasher.php, to allow manual usage of the hashing logic, regardless of monolog

--- src/HashSensitiveProcessor.php
+++ src/HashSensitiveProcessor.php
@@ -12,12 +12,9 @@ use UnexpectedValueException;
  * The processor to be added to your Monolog instance.
  * @package HashSensitive
  */
-class HashSensitiveProcessor implements ProcessorInterface
+class HashSensitiveProcessor extends Hasher implements ProcessorInterface
 {
     private array $sensitiveKeys;
-    private ?int $lengthLimit;
-    private string $algorithm;
-    private bool $exclusiveSubtree;
 
     /**
      * Creates a new HashSensitiveProcessor instance.
@@ -28,9 +25,8 @@ class HashSensitiveProcessor implements ProcessorInterface
     public function __construct(array $sensitiveKeys, string $algorithm = 'sha256', ?int $lengthLimit = null, bool $exclusiveSubtree = true)
     {
         $this->sensitiveKeys = $sensitiveKeys;
-        $this->lengthLimit = $lengthLimit;
-        $this->algorithm = $algorithm;
-        $this->exclusiveSubtree = $exclusiveSubtree;
+
+        parent::__construct($algorithm, $lengthLimit, $exclusiveSubtree);
     }
 
     /**
@@ -44,129 +40,5 @@ class HashSensitiveProcessor implements ProcessorInterface
     {
         $redactedContext = $this->traverseInputArray($record->context, $this->sensitiveKeys);
         return $record->with(context: $redactedContext);
-    }
-
-    /**
-     * Function to hash the input value, using the specified hashing algorithm and length limit
-     *
-     * @param string $value The value to hash
-     *
-     * @return string|null The hashed value, or null, if the input string was empty
-     */
-    private function hash(string $value): ?string
-    {
-        if (strlen($value) === 0) {
-            return null;
-        }
-
-        // Cut the input to the length limit specified
-        $cutInput = substr($value, 0, $this->lengthLimit);
-
-        if (strlen($cutInput) === 0) {
-            return null;
-        }
-
-        return hash($this->algorithm, $cutInput);
-    }
-
-    /**
-     * Function to handle traversing arrays and objects
-     *
-     * @param string       $key           The key being processed
-     * @param array|object $value         The value of the key in the input data
-     * @param array        $sensitiveKeys The list of keys to hash
-     *
-     * @throws UnexpectedValueException if $value was not either an array of an object
-     *
-     * @return array|object The processed array or object
-     */
-    private function traverse(string $key, array|object $value, array $sensitiveKeys): array|object
-    {
-        if (is_array($value)) {
-            return $this->traverseInputArray($value, $sensitiveKeys);
-        }
-
-        if (is_object($value)) {
-            return $this->traverseObject($value, $sensitiveKeys);
-        }
-
-        throw new UnexpectedValueException("Don't know how to traverse value at key $key");
-    }
-
-    /**
-     * Traverse an array and replace all values to be redacted with a hashed version of the value
-     *
-     * @param array $inputArray    Array to redact values from
-     * @param array $sensitiveKeys Keys to redact
-     *
-     * @return array Input array with redacted values hashed
-     */
-    public function traverseInputArray(array $inputArray, array $sensitiveKeys): array
-    {
-        foreach ($inputArray as $key => $value) {
-            if ($value === null) {
-                // Nothing to hash or process
-                continue;
-            }
-
-            // If the value is not an array or an object, hash it if it is a sensitive key
-            if (is_scalar($value)) {
-                if (in_array($key, $sensitiveKeys) || array_key_exists($key, $sensitiveKeys)) {
-                    $inputArray[$key] = $this->hash((string) $value);
-                }
-
-                continue;
-            }
-
-            // The value is either an array or an object, let traverse handle the specifics
-            if (in_array($key, $sensitiveKeys) || array_key_exists($key, $sensitiveKeys)) {
-                $inputArray[$key] = $this->traverse($key, $value, $sensitiveKeys[$key]);
-
-                // ExclusiveSubtree turned off means that sub keys should be checked according to ALL keys, not just
-                // the keys in their sensitive keys subtree
-                if (!$this->exclusiveSubtree) {
-                    $inputArray[$key] = $this->traverse($key, $inputArray[$key], $sensitiveKeys);
-                }
-            } else {
-                $inputArray[$key] = $this->traverse($key, $value, $sensitiveKeys);
-            }
-        }
-
-        return $inputArray;
-    }
-
-    /**
-     * Traverse an object and replace all values to be redacted with a hashed version of the value
-     *
-     * @param object $object        Object to redact values from
-     * @param array  $sensitiveKeys Keys for which to hash the value
-     *
-     * @return object The object with redacted values hashed
-     */
-    private function traverseObject(object $object, array $sensitiveKeys): object
-    {
-        foreach (get_object_vars($object) as $key => $value) {
-            // If the value is not an array or an object, hash it if it is a sensitive key
-            if (is_scalar($value)) {
-                if (in_array($key, $sensitiveKeys) || array_key_exists($key, $sensitiveKeys)) {
-                    $object->{$key} = $this->hash((string) $value);
-                }
-
-                continue;
-            }
-
-            // The value is either an array or an object, let traverse handle the specifics
-            if (in_array($key, $sensitiveKeys) || array_key_exists($key, $sensitiveKeys)) {
-                $object->{$key} = $this->traverse($key, $value, $sensitiveKeys[$key]);
-
-                if (!$this->exclusiveSubtree) {
-                    $object->{$key} = $this->traverse($key, $object->{$key}, $sensitiveKeys);
-                }
-            } else {
-                $object->{$key} = $this->traverse($key, $value, $sensitiveKeys);
-            }
-        }
-
-        return $object;
     }
 }
---
+++ src/Hasher.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace HashSensitive;
+
+/**
+ * Class to manage scrubbing the keys from an array
+ *
+ * @author sjustein
+ */
+class Hasher
+{
+    private ?int $lengthLimit;
+    private string $algorithm;
+    private bool $exclusiveSubtree;
+
+    public function __construct(string $algorithm = 'sha256', ?int $lengthLimit = null, bool $exclusiveSubtree = true)
+    {
+        $this->algorithm = $algorithm;
+        $this->lengthLimit = $lengthLimit;
+        $this->exclusiveSubtree = $exclusiveSubtree;
+    }
+
+    /**
+     * Function to hash sensitive keys in an input array
+     *
+     * @param array $inputArray    The array to hash values in
+     * @param array $sensitiveKeys The keys to hash
+     *
+     * @return array The input array with sensitive keys hashed
+     */
+    public function scrubKeys(array $inputArray, array $sensitiveKeys): array
+    {
+        return $this->traverseInputArray($inputArray, $sensitiveKeys);
+    }
+
+    /**
+     * Function to hash the input value, using the specified hashing algorithm and length limit
+     *
+     * @param string $value The value to hash
+     *
+     * @return string|null The hashed value, or null, if the input string was empty
+     */
+    protected function hash(string $value): ?string
+    {
+        if (strlen($value) === 0) {
+            return null;
+        }
+
+        // Cut the input to the length limit specified
+        $cutInput = substr($value, 0, $this->lengthLimit);
+
+        if (strlen($cutInput) === 0) {
+            return null;
+        }
+
+        return hash($this->algorithm, $cutInput);
+    }
+
+    /**
+     * Function to handle traversing arrays and objects
+     *
+     * @param string       $key           The key being processed
+     * @param array|object $value         The value of the key in the input data
+     * @param array        $sensitiveKeys The list of keys to hash
+     *
+     * @throws UnexpectedValueException if $value was not either an array of an object
+     *
+     * @return array|object The processed array or object
+     */
+    protected function traverse(string $key, array|object $value, array $sensitiveKeys): array|object
+    {
+        if (is_array($value)) {
+            return $this->traverseInputArray($value, $sensitiveKeys);
+        }
+
+        if (is_object($value)) {
+            return $this->traverseObject($value, $sensitiveKeys);
+        }
+
+        throw new UnexpectedValueException("Don't know how to traverse value at key $key");
+    }
+
+    /**
+     * Traverse an array and replace all values to be redacted with a hashed version of the value
+     *
+     * @param array $inputArray    Array to redact values from
+     * @param array $sensitiveKeys Keys to redact
+     *
+     * @return array Input array with redacted values hashed
+     */
+    protected function traverseInputArray(array $inputArray, array $sensitiveKeys): array
+    {
+        foreach ($inputArray as $key => $value) {
+            if ($value === null) {
+                // Nothing to hash or process
+                continue;
+            }
+
+            // If the value is not an array or an object, hash it if it is a sensitive key
+            if (is_scalar($value)) {
+                if (in_array($key, $sensitiveKeys) || array_key_exists($key, $sensitiveKeys)) {
+                    $inputArray[$key] = $this->hash((string) $value);
+                }
+
+                continue;
+            }
+
+            // The value is either an array or an object, let traverse handle the specifics
+            if (in_array($key, $sensitiveKeys) || array_key_exists($key, $sensitiveKeys)) {
+                $inputArray[$key] = $this->traverse($key, $value, $sensitiveKeys[$key]);
+
+                // ExclusiveSubtree turned off means that sub keys should be checked according to ALL keys, not just
+                // the keys in their sensitive keys subtree
+                if (!$this->exclusiveSubtree) {
+                    $inputArray[$key] = $this->traverse($key, $inputArray[$key], $sensitiveKeys);
+                }
+            } else {
+                $inputArray[$key] = $this->traverse($key, $value, $sensitiveKeys);
+            }
+        }
+
+        return $inputArray;
+    }
+
+    /**
+     * Traverse an object and replace all values to be redacted with a hashed version of the value
+     *
+     * @param object $object        Object to redact values from
+     * @param array  $sensitiveKeys Keys for which to hash the value
+     *
+     * @return object The object with redacted values hashed
+     */
+    protected function traverseObject(object $object, array $sensitiveKeys): object
+    {
+        foreach (get_object_vars($object) as $key => $value) {
+            // If the value is not an array or an object, hash it if it is a sensitive key
+            if (is_scalar($value)) {
+                if (in_array($key, $sensitiveKeys) || array_key_exists($key, $sensitiveKeys)) {
+                    $object->{$key} = $this->hash((string) $value);
+                }
+
+                continue;
+            }
+
+            // The value is either an array or an object, let traverse handle the specifics
+            if (in_array($key, $sensitiveKeys) || array_key_exists($key, $sensitiveKeys)) {
+                $object->{$key} = $this->traverse($key, $value, $sensitiveKeys[$key]);
+
+                if (!$this->exclusiveSubtree) {
+                    $object->{$key} = $this->traverse($key, $object->{$key}, $sensitiveKeys);
+                }
+            } else {
+                $object->{$key} = $this->traverse($key, $value, $sensitiveKeys);
+            }
+        }
+
+        return $object;
+    }
+}
